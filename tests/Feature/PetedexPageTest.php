7cbc1218ec2286e7bf491bec0fef600770e6a3cf
Marcus Nightingale <marcusnightingale.1@gmail.com>
2024-10-23 09:23:19 +0100

    - adding in a simple test

---
+++ tests/Feature/PetedexPageTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PetedexPageTest extends TestCase
+{
+    /**
+     * Test that the Petédex page loads successfully and returns a 200 status.
+     */
+    public function testPageLoadsSuccessfully()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test that the title is 'Petédex | Home'.
+     */
+    public function testTitleIsCorrect()
+    {
+        $response = $this->get('/');
+        $response->assertSeeInOrder([
+            '<title>Petédex | Home</title>'
+        ]);
+    }
+
+    /**
+     * Test that the page contains the search input.
+     */
+    public function testSearchBarExists()
+    {
+        $response = $this->get('/');
+        $response->assertSee('id="searchInput"', false);
+    }
+}
